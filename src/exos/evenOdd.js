f1039ca5f0e1c399113144c0c6986045b978d8aa
horameus <ludo.hartmann@gmail.com>
2025-05-14 00:38:39 +0200

    evenOdd medium

--- src/exos/evenOdd.js
+++ src/exos/evenOdd.js
@@ -2,3 +2,25 @@ export function evenOddEasy(number) {
     if (number % 2 === 0) return 'even';
     else return 'odd';
 }
+
+export function evenOddMedium(numArray) {
+    let even = 0,
+        odd = 0;
+    for (let i = 0; i < numArray.length; i++) {
+        numArray[i] % 2 === 0 ? even++ : odd++;
+    }
+    return {
+        even: even,
+        odd: odd,
+    };
+}
+
+export function evenOddMediumAlt(numArray) {
+    return numArray.reduce(
+        (acc, num) => {
+            num % 2 === 0 ? acc.even++ : acc.odd++;
+            return acc;
+        },
+        { even: 0, odd: 0 },
+    );
+}
--- src/exos/evenOdd.test.js
+++ src/exos/evenOdd.test.js
@@ -1,5 +1,5 @@
 import { test, expect } from 'vitest';
-import { evenOddEasy } from './evenOdd';
+import { evenOddEasy, evenOddMedium } from './evenOdd';
 
 test('param number should return even', () => {
     expect(evenOddEasy(18)).toBe('even');
@@ -7,4 +7,13 @@ test('param number should return even', () => {
 
 test('param number should return odd', () => {
     expect(evenOddEasy(37)).toBe('odd');
+});
+
+const numArray = [1, 17, 45, 12, 19, 10];
+
+test('should return number of odd and even number', () => {
+    expect(evenOddMedium(numArray)).toStrictEqual({
+        even: 2,
+        odd: 4,
+    });
 });
