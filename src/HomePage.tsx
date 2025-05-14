fc1b9cc4dba2adab7ddda5c42475f54c4eeb4601
horameus <ludo.hartmann@gmail.com>
2025-05-14 15:04:14 +0200

    size observer

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -1,12 +1,18 @@
 import { Link, Outlet } from 'react-router-dom';
 import { ToggleButton } from '@/components/ToggleButton';
+import { useResizeObserver } from '@/utilities/useResizeObserver';
 
 export function HomePage() {
+    const { width, height } = useResizeObserver();
+
     return (
         <>
             <div className="homepage">
                 <h1>Welcome to the React App</h1>
                 <p>This is a simple React application.</p>
+                <p>Current window size</p>
+                <p>width : {width}</p>
+                <p>height : {height}</p>
                 <ul>
                     <li>
                         <Link to="/profiles">Profiles List</Link>
---
+++ src/utilities/useResizeObserver.ts
@@ -0,0 +1,24 @@
+import { useEffect, useState } from 'react';
+
+export function useResizeObserver() {
+    const [size, setSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+    });
+
+    useEffect(() => {
+        const observer = new ResizeObserver(() => {
+            setSize({
+                width: window.innerWidth,
+                height: window.innerHeight,
+            });
+        });
+
+        observer.observe(document.body);
+        return () => {
+            observer.disconnect();
+        };
+    }, []);
+
+    return size;
+}
