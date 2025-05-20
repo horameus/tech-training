44f597b0550cbec4c106cdc6baf664a90710f35f
horameus <ludo.hartmann@gmail.com>
2025-05-20 22:21:10 +0200

    isogram

--- src/exos/bulk/bulk.js
+++ src/exos/bulk/bulk.js
@@ -11,3 +11,39 @@ export function countVowels(str) {
 
     return count;
 }
+
+export function countVowelsAlt(str) {
+    const vowels = new Set(['a', 'e', 'i', 'o', 'u', 'y']);
+    let count = 0;
+
+    for (let char of str.toLowerCase()) {
+        if (vowels.has(char)) count++;
+    }
+
+    return count;
+}
+
+// Task: Check if a word is an isogram (a word with no repeating letters). Ignore case.
+export function isIsogram(str) {
+    const word = str.toLowerCase().split('');
+
+    for (let i = 0; i < word.length; i++) {
+        const result = word.filter(letter => letter === word[i]);
+        if (result.length > 1) return false;
+    }
+
+    return true;
+}
+
+export function isIsogramAlt(str) {
+    const seen = new Set();
+
+    for (const char of str.toLowerCase()) {
+        if (seen.has(char)) {
+            return false;
+        }
+        seen.add(char);
+    }
+
+    return true;
+}
--- src/exos/bulk/bulk.test.js
+++ src/exos/bulk/bulk.test.js
@@ -1,6 +1,10 @@
 import { expect, test } from 'vitest';
-import { countVowels } from './bulk.js';
+import { countVowels, isIsogram } from './bulk.js';
 
 test('should return the number of vowels in a string', () => {
     console.log(countVowels('Hello world Its a me, Mario!'));
+});
+
+test('isogram', () => {
+    console.log(isIsogram('Machine'));
 });
